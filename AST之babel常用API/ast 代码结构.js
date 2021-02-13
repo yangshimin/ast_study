a7f4b46f76f69e29b54ef8333d8c94f756f3259f
yangshimin <843113495a@gmail.com>
2021-02-13 23:39:53 +0800

    安装在项目目录下可以查看源码

---
+++ AST之babel常用API/ast 代码结构.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+// 解析js,将js代码转换为AST
+// parser的parse方法有第二个参数sourceType, 当js代码中含有import、export (es6的代码)等关键字时
+// 需要指定sourceType为module
+const parser = require("@babel/parser");
+// 用来遍历AST中的节点
+const traverse = require("@babel/traverse").default;
+// 用来判断节点类型和生成新的节点等
+const t = require("@babel/types");
+// 用来把AST代码转换成JS代码
+// generator 也有其他参数，具体参考文档: https://babeljs.io/docs/en/@babel-generator
+const generator = require("@babel/generator").default;
+
+const js_code = fs.readFileSync("F:\\code\\ast_study\\demo\\demo1.js", {
+    encoding: "utf-8"
+});
+let ast = parser.parse(js_code);
+
+// 在这里对AST进行一系列的操作
+
+let code = generator(ast).code;
+fs.writeFile('./AST之babel常用API/demoNew.js', code, (err)=>{});
