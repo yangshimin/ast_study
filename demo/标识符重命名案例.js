6982064808f21a0314cb777b90051ce51f884670
yangshimin <843113495a@gmail.com>
2021-02-20 22:43:40 +0800

    标识符重命名案例

---
+++ demo/标识符重命名案例.js
@@ -0,0 +1,23 @@
+var b = 200;
+var c = 300;
+
+var abc = function () {
+    var a = 1000;
+};
+abc();
+
+// path.scope.getOwnBinding('bcd') 获取到的绑定为underfined
+// 但在调用的时候可以获取到绑定
+function bcd() {
+    var a = 1000;
+    var b = 500;
+    function test(){
+        var a = 200;
+        var c = 200;
+        return a + c + b;
+    }
+    test();
+    return a + b + c;
+}
+
+bcd();
